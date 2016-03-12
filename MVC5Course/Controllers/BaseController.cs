a3e40c9129f3492eaacd23e051839a4b423e8a4d
gogo1119 <gogo1119@gmail.com>
2016-03-12 13:33:29 +0800

    加入BaseController

---
+++ MVC5Course/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        /// <summary>
+        /// The repo
+        /// </summary>
+        protected ProductRepository Repo = RepositoryHelper.GetProductRepository();
+
+        /// <summary>
+        /// 當要求符合這個控制器，但在控制器中找不到指定動作名稱的方法時呼叫。
+        /// </summary>
+        /// <param name="actionName">嘗試之動作的名稱。</param>
+        protected override void HandleUnknownAction(string actionName)
+        {
+            this.Redirect("/").ExecuteResult(this.ControllerContext);
+            //base.HandleUnknownAction(actionName);
+        }
+    }
+}
