ecfeec4f88e4b23e3f80e5bbe5134500172c3567
gogo1119 <gogo1119@gmail.com>
2016-03-19 16:20:40 +0800

    加入分頁

---
+++ MVC5Course/Controllers/AjaxTestController.cs
@@ -0,0 +1,39 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        // GET: AjaxTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Product vmProduct)
+        {
+            //寫入資料庫
+            //取得序號
+            ViewData["serial"] = 10;
+
+            //重新取得資料
+            FabricsEntities db = new FabricsEntities();
+            var data = db.Product.Take(1).ToList()[0];
+
+            //return RedirectToAction("ConfirmView", new { data });
+            return View("ConfirmView", data);
+        }
+
+        public ActionResult ConfirmView(Product data)
+        {
+            //var b = ViewData["serial"].ToString();
+            return View(data);
+        }
+    }
+}
